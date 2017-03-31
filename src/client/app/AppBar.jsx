3434f76b0e7ac50b0c3ecb24f9cb0fe50d6c3256
shakti lucifer <shakticse@gmail.com>
2017-03-30 23:41:34 -0400

    bug fix

--- src/client/app/AppBar.jsx
+++ src/client/app/AppBar.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 import AppBar from 'material-ui/AppBar';
 let Button = require('./Button.jsx');
 
-const AppBarExampleIcon = () => (
-  <li>
-    <div 
-    style={{backgroundColor:'grey',color:'white'}} 
-    className="collapsible-header">
-      <b>  RECIPE BOOK </b>
-    </div>
-  </li>
-);
+var AppBarExampleIcon = React.createClass({
+  render() {
+    return (
+      <li>
+        <div
+          style={{ backgroundColor: 'grey', color: 'white' }}
+          className="collapsible-header">
+          <b>  RECIPE BOOK </b>
+
+        </div>
+      </li>
+    )
+  }
+});
 
 export default AppBarExampleIcon;
