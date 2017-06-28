ec66eb072f145f589d6823d36a64ef81386604f2
Shakti <shakticse@gmail.com>
2017-06-27 23:22:03 -0400

    trying new formatter

--- src/client/app/AddRecipeForm.jsx
+++ src/client/app/AddRecipeForm.jsx
@@ -2,28 +2,35 @@ import React from 'react';
 import TextField from 'material-ui/TextField';
 
 var AddRecipeForm = React.createClass({
-  render() {
-    return (
-      <div>
-        <TextField
-          name="recipeName"
-          value={this.props.recipeName}
-          onChange={this.props.onChange}
-          floatingLabelFixed={true}
-          floatingLabelText="Recipe Name" />
-          <br/>
-        <TextField
-          name="ingredients"
-          value={this.props.ingredients}
-          onChange={this.props.onChange}
-          floatingLabelFixed={true}
-          floatingLabelText="Ingredients" />
-      </div>
-    )
-  }
+ render() {
+  return ( < div >
+   < TextField name = "recipeName"
+   value = {
+    this.props.recipeName
+   }
+   onChange = {
+    this.props.onChange
+   }
+   floatingLabelFixed = {
+    true
+   }
+   floatingLabelText = "Recipe Name" / >
+   < br / >
+   < TextField name = "ingredients"
+   value = {
+    this.props.ingredients
+   }
+   onChange = {
+    this.props.onChange
+   }
+   floatingLabelFixed = {
+    true
+   }
+   floatingLabelText = "Ingredients" / >
+   < /div>
+  )
+ }
 
 });
 
 module.exports = AddRecipeForm;
-
-
